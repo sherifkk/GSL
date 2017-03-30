48db5637f2dbdf5f0c2a83c71c5e7750221052f0
Sherif Shrf <msherifkk@gmail.com>
2017-03-30 23:12:00 +0530

    Conrdinator Tab Layout

--- app/src/main/java/org/gec/cs/gsl/MainActivity.java
+++ app/src/main/java/org/gec/cs/gsl/MainActivity.java
@@ -2,24 +2,37 @@ package org.gec.cs.gsl;
 
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
-import android.view.View;
 import android.support.design.widget.NavigationView;
+import android.support.design.widget.Snackbar;
+import android.support.v4.app.Fragment;
 import android.support.v4.view.GravityCompat;
+import android.support.v4.view.ViewPager;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import cn.hugeterry.coordinatortablayout.CoordinatorTabLayout;
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener {
+
+  private CoordinatorTabLayout mCoordinatorTabLayout;
+  private int[] mImageArray, mColorArray;
+  private ArrayList<Fragment> mFragments;
+  private final String[] mTitles = {"Android", "iOS", "前端", "拓展资源"};
+  private ViewPager mViewPager;
 
   @Override protected void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
     setContentView(R.layout.activity_main);
     Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
     setSupportActionBar(toolbar);
+
+    initFragments();
+    initViewPager();
 
     FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
     fab.setOnClickListener(new View.OnClickListener() {
@@ -48,6 +61,20 @@ public class MainActivity extends AppCompatActivity implements NavigationView.On
       super.onBackPressed();
     }
   }
+
+  private void initFragments() {
+    mFragments = new ArrayList<>();
+    for (String title : mTitles) {
+      mFragments.add(MainFragment.getInstance(title));
+    }
+  }
+
+  private void initViewPager() {
+    mViewPager = (ViewPager) findViewById(R.id.vp);
+    mViewPager.setOffscreenPageLimit(4);
+    mViewPager.setAdapter(new MyPagerAdapter(getSupportFragmentManager(), mFragments, mTitles));
+  }
+
 
   @Override public boolean onCreateOptionsMenu(Menu menu) {
     // Inflate the menu; this adds items to the action bar if it is present.
